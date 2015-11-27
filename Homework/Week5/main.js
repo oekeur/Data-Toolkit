e0e2ccb382054f25a2ac9f76051f77119c6a0e8d
oekeur <o.e.keur@gmail.com>
2015-11-27 13:12:42 +0100

    exercise 1, manually change

--- Homework/Week5/main.js
+++ Homework/Week5/main.js
@@ -1,16 +1,37 @@
 /* use this to test out your function */
 window.onload = function() {
- 	// translateContinents();
+ 	Continents();
  	// changeColor();
 }
 
-function translateContinents () {
+continents = {
+	AS : [],
+	EU : [],
+	NA : [],
+	SA : [],
+	OC : []
+}
+
+function Continents () {
+	json = JSON.parse(document.getElementById('countries').innerHTML);
+	for (var i = 0; i < json.country.length; i++) {
+		if (json.country[i].continent == "EU") {
+			continents.EU.push(json.country[i].countryCode.toLowerCase())};
+		if (json.country[i].continent == "AS") {
+			continents.AS.push(json.country[i].countryCode.toLowerCase())};
+		if (json.country[i].continent == "NA") {
+			continents.NA.push(json.country[i].countryCode.toLowerCase())};
+		if (json.country[i].continent == "SA") {
+			continents.SA.push(json.country[i].countryCode.toLowerCase())};
+		if (json.country[i].continent == "OC") {
+			continents.OC.push(json.country[i].countryCode.toLowerCase())};
+	}
 	
 }
 
 
 /* changeColor takes a path ID and a color (hex value)
    and changes that path's fill color */
-function changeColor(id, color) {
+// function changeColor(id, color) {
         
-}
+// }
