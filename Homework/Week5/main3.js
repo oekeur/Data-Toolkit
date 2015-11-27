57d213e057be5cff576486f026d8ec57ebfebd44
oekeur <o.e.keur@gmail.com>
2015-11-27 15:32:13 +0100

    Exercise 3, data visualization
    Nog bugfree yet

--- Homework/Week5/main3.js
+++ Homework/Week5/main3.js
@@ -1,10 +1,8 @@
 /* use this to test out your function */
 window.onload = function() {
  	Continents();
-	changeColor("id" , "#33cc33")
-	changeColor("aq" , "#33cc33")
-	changeColor("sd" , "#33cc33")
-	changeColor("gb" , "#33cc33")
+ 	Data();
+ 	// changeColor("us", "#000099")
 }
 
 continents = {
@@ -14,6 +12,8 @@ continents = {
 	SA : [],
 	OC : []
 }
+
+data = []
 
 function Continents () {
 	json = JSON.parse(document.getElementById('countries').innerHTML);
@@ -28,6 +28,31 @@ function Continents () {
 			continents.SA.push(json.country[i].countryCode.toLowerCase())};
 		if (json.country[i].continent == "OC") {
 			continents.OC.push(json.country[i].countryCode.toLowerCase())};
+	}
+	
+}
+
+function Data () {
+	json2 = JSON.parse(document.getElementById('data').innerHTML);
+	for (var i = 0; i < json2.length; i++) {
+		data.push(Number(json2[i].value))
+	}
+	data.sort(function(a, b){return a-b});
+
+
+	for (var i = 0; i < json.country.length; i++) {
+		if (json2[i].value <= 50) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#f7f7f7")};
+		if (json2[i].value > 50 && json2[i].value <= 100) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#d9d9d9")};
+		if (json2[i].value > 100 && json2[i].value <= 200) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#bdbdbd")};
+		if (json2[i].value > 200 && json2[i].value <= 400) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#969696")};
+		if (json2[i].value > 400 && json2[i].value <= 1000) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#636363")};
+		if (json2[i].value > 1000) {
+			changeColor(json.country[i].countryCode.toLowerCase(), "#252525")};
 	}
 	
 }
