bcbc3e8430d2b8706e3928aeadd9ecc59bacd7ee
oekeur <o.e.keur@gmail.com>
2015-11-27 10:56:03 +0100

    second commit
    Map embedded, JSON data for codes + continents

--- Homework/Week5/main.js
+++ Homework/Week5/main.js
@@ -1,7 +1,13 @@
 /* use this to test out your function */
 window.onload = function() {
+ 	// translateContinents();
  	// changeColor();
 }
+
+function translateContinents () {
+	
+}
+
 
 /* changeColor takes a path ID and a color (hex value)
    and changes that path's fill color */
